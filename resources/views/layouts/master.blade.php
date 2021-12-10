b16b508b76e40005fda3eed59548814582fec38b
Deniz Oral <denizoral299@gmail.com>
2021-12-10 23:38:27 +0000

    added the login and register page and a navbar

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            <span class="fs-4">Facebook</span>
+          </a>
+          @if (Auth::check())
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Notifications</a></li>
+            <li class="nav-item"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">Logout</a></li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+          </ul>
+          @else
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
+            @endif
+        </header>
+      </div>
+      
+    @yield('content')
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="/css/styles.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
-</head>
-<body>
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#oka">placeholder</a>
-        <a href="#oka">placeholder</a>
-        <a href="#oka">placeholder</a>
-        <a style="float: right;" href="#notifications">Notifications</a>
-        <a style="float: right;" href="#settings">Settings</a>
-    </div>
+@extends('layouts.master')
+@section('title homepage')
 
+@section('content')
+
+    <link href="/css/styles.css" rel="stylesheet">
+    @if (Auth::check())
     <div class="addPost">
         <h5>Send a post down below</h5>
         <input class="form-control" type="text" placeholder="Default input">
         <input type="button" id="sendBtn" placeholder="Add Post">
     </div>
+    @else
+    <div class="addPost">
+        <h5>Log in or register to be able to send posts</h5>
+    </div>
+    @endif
 
 
     @foreach ($posts as $post)
@@ -41,7 +33,4 @@
 
         </div>
     @endforeach
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
